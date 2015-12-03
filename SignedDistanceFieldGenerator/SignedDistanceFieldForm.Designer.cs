74b1018a99011bb4a2170c0c7a1e34879c791775
insanepure <insanepure@gmail.com>
2015-12-03 22:05:56 +0100

    Version 1.0
    SDF Generation from monochromatical images
    Define Size and Inner
    Color
    Multithreaded

---
+++ SignedDistanceFieldGenerator/SignedDistanceFieldForm.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace SignedDistanceFieldGenerator
+{
+    partial class SignedDistanceFieldForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadButton = new System.Windows.Forms.Button();
+            this.imgpath = new System.Windows.Forms.Label();
+            this.imagebox = new System.Windows.Forms.PictureBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.colorBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progress = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.searchRadius = new System.Windows.Forms.TrackBar();
+            this.MinRadius = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Radius = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.OutWidth = new System.Windows.Forms.NumericUpDown();
+            this.OutHeight = new System.Windows.Forms.NumericUpDown();
+            this.timeLeft = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imagebox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchRadius)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutHeight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(12, 27);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // imgpath
+            // 
+            this.imgpath.AutoSize = true;
+            this.imgpath.Location = new System.Drawing.Point(208, 86);
+            this.imgpath.Name = "imgpath";
+            this.imgpath.Size = new System.Drawing.Size(0, 13);
+            this.imgpath.TabIndex = 1;
+            // 
+            // imagebox
+            // 
+            this.imagebox.Location = new System.Drawing.Point(211, 102);
+            this.imagebox.Name = "imagebox";
+            this.imagebox.Size = new System.Drawing.Size(249, 255);
+            this.imagebox.TabIndex = 2;
+            this.imagebox.TabStop = false;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(288, 402);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(102, 23);
+            this.generateButton.TabIndex = 4;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(385, 27);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 5;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 448);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(448, 23);
+            this.progressBar.TabIndex = 6;
+            // 
+            // colorBox
+            // 
+            this.colorBox.BackColor = System.Drawing.Color.Black;
+            this.colorBox.Location = new System.Drawing.Point(46, 118);
+            this.colorBox.Name = "colorBox";
+            this.colorBox.Size = new System.Drawing.Size(115, 50);
+            this.colorBox.TabIndex = 7;
+            this.colorBox.TabStop = false;
+            this.colorBox.Click += new System.EventHandler(this.colorBox_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Inner Color";
+            // 
+            // progress
+            // 
+            this.progress.AutoSize = true;
+            this.progress.Location = new System.Drawing.Point(13, 429);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(0, 13);
+            this.progress.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Search Radius";
+            // 
+            // searchRadius
+            // 
+            this.searchRadius.LargeChange = 1;
+            this.searchRadius.Location = new System.Drawing.Point(46, 249);
+            this.searchRadius.Maximum = 100;
+            this.searchRadius.Name = "searchRadius";
+            this.searchRadius.Size = new System.Drawing.Size(115, 45);
+            this.searchRadius.TabIndex = 14;
+            this.searchRadius.Scroll += new System.EventHandler(this.searchRadius_Scroll);
+            // 
+            // MinRadius
+            // 
+            this.MinRadius.AutoSize = true;
+            this.MinRadius.Location = new System.Drawing.Point(31, 249);
+            this.MinRadius.Name = "MinRadius";
+            this.MinRadius.Size = new System.Drawing.Size(13, 13);
+            this.MinRadius.TabIndex = 15;
+            this.MinRadius.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(157, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "100";
+            // 
+            // Radius
+            // 
+            this.Radius.AutoSize = true;
+            this.Radius.Location = new System.Drawing.Point(94, 280);
+            this.Radius.Name = "Radius";
+            this.Radius.Size = new System.Drawing.Size(13, 13);
+            this.Radius.TabIndex = 17;
+            this.Radius.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(52, 280);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Radius:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 317);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Width";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(88, 317);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Height";
+            // 
+            // OutWidth
+            // 
+            this.OutWidth.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.OutWidth.Location = new System.Drawing.Point(12, 333);
+            this.OutWidth.Maximum = new decimal(new int[] {
+            8096,
+            0,
+            0,
+            0});
+            this.OutWidth.Minimum = new decimal(new int[] {
+            48,
+            0,
+            0,
+            0});
+            this.OutWidth.Name = "OutWidth";
+            this.OutWidth.Size = new System.Drawing.Size(64, 20);
+            this.OutWidth.TabIndex = 23;
+            this.OutWidth.Value = new decimal(new int[] {
+            48,
+            0,
+            0,
+            0});
+            // 
+            // OutHeight
+            // 
+            this.OutHeight.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.OutHeight.Location = new System.Drawing.Point(91, 333);
+            this.OutHeight.Maximum = new decimal(new int[] {
+            8096,
+            0,
+            0,
+            0});
+            this.OutHeight.Minimum = new decimal(new int[] {
+            48,
+            0,
+            0,
+            0});
+            this.OutHeight.Name = "OutHeight";
+            this.OutHeight.Size = new System.Drawing.Size(64, 20);
+            this.OutHeight.TabIndex = 24;
+            this.OutHeight.Value = new decimal(new int[] {
+            48,
+            0,
+            0,
+            0});
+            // 
+            // timeLeft
+            // 
+            this.timeLeft.AutoSize = true;
+            this.timeLeft.Location = new System.Drawing.Point(12, 478);
+            this.timeLeft.Name = "timeLeft";
+            this.timeLeft.Size = new System.Drawing.Size(0, 13);
+            this.timeLeft.TabIndex = 25;
+            // 
+            // SignedDistanceFieldForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 513);
+            this.Controls.Add(this.timeLeft);
+            this.Controls.Add(this.OutHeight);
+            this.Controls.Add(this.OutWidth);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Radius);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.MinRadius);
+            this.Controls.Add(this.searchRadius);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.colorBox);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.imagebox);
+            this.Controls.Add(this.imgpath);
+            this.Controls.Add(this.loadButton);
+            this.Name = "SignedDistanceFieldForm";
+            this.Text = "Signed Distance Field Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.imagebox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchRadius)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutHeight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Label imgpath;
+        private System.Windows.Forms.PictureBox imagebox;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.PictureBox colorBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label progress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar searchRadius;
+        private System.Windows.Forms.Label MinRadius;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Radius;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown OutWidth;
+        private System.Windows.Forms.NumericUpDown OutHeight;
+        private System.Windows.Forms.Label timeLeft;
+    }
+}
+
---
+++ SignedDistanceFieldGenerator/SignedDistanceFieldForm.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignedDistanceFieldGenerator
+{
+    struct Job
+    {
+        public int minX;
+        public int minY;
+        public int maxX;
+        public int maxY;
+    }
+
+    public partial class SignedDistanceFieldForm : Form
+    {
+
+        #region Variables
+        private string imgPath = "";
+
+        private Bitmap loadedImage;
+
+        private OpenFileDialog readDialog;
+        private SaveFileDialog saveDialog;
+        private int running;
+
+        private const int jobsHorizontal = 10;
+        private const int jobsVertical = 10;
+        private int numCores = Environment.ProcessorCount;
+        private const int jobsCount = jobsHorizontal * jobsVertical;
+        private int jobMaxWidth = 0;
+        private int jobMaxHeight = 0;
+        private int currentJob = 0;
+        static readonly object jobLocker = new object();
+        private int radius = 0;
+
+        float maxValue = 0;
+        float currentValue = 0;
+        DateTime startTime;
+
+        List<Job> Jobs = new List<Job>();
+
+        #endregion
+
+        public SignedDistanceFieldForm()
+        {
+            InitializeComponent();
+            readDialog = new OpenFileDialog();
+            readDialog.Title = "Load an Image";
+            readDialog.Filter = "PNG|*.png";
+            readDialog.InitialDirectory = @"C:\";
+
+            saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "PNG|*.png";
+
+            running = 0;
+
+            progress.Text = "Load a File";
+        }
+
+        public static float Clamp(float value, float min, float max)
+        {
+            return (value < min) ? min : (value > max) ? max : value;
+        }
+
+        private void CopyToImage(ref byte[] pixelArray, ref Bitmap Image)
+        {
+            BitmapData imageData = Image.LockBits(new Rectangle(0, 0, Image.Width, Image.Height), ImageLockMode.WriteOnly, Image.PixelFormat);
+
+            //copy the calculations into the out image
+            IntPtr ptrFirstPixel = imageData.Scan0;
+            Marshal.Copy(pixelArray, 0, ptrFirstPixel, pixelArray.Length);
+
+            Image.UnlockBits(imageData);
+        }
+
+        private void GeneratePixelArray(ref Bitmap Image, out byte[] pixelArray, bool copy)
+        {
+            lock(jobLocker)
+            {
+                BitmapData imageData = Image.LockBits(new Rectangle(0, 0, Image.Width, Image.Height), ImageLockMode.ReadOnly, Image.PixelFormat);
+
+                int bytesPerPixel = Bitmap.GetPixelFormatSize(Image.PixelFormat) / 8;
+                int byteCount = imageData.Stride * Image.Height;
+                //create variables
+                pixelArray = new byte[byteCount];
+
+                if (copy)
+                {
+                    //Copy Data
+                    IntPtr ptrFirstPixel = imageData.Scan0;
+                    Marshal.Copy(ptrFirstPixel, pixelArray, 0, pixelArray.Length);
+                }
+
+                Image.UnlockBits(imageData);
+            }
+        }    
+
+        private void SignedDistanceField(int sx, int sy, int ex, int ey, ref byte[] threadPixels, ref byte[] writePixels)
+        {
+            Color InColor = Color.FromArgb(255, colorBox.BackColor.R, colorBox.BackColor.G, colorBox.BackColor.B);
+
+            for (int y = sy; y < ey; ++y)
+            {
+                for (int x = sx; x < ex; ++x)
+                {
+
+                    int idx = (x + (y * jobMaxWidth)) * 4;
+
+                    float distance = float.MaxValue;
+                    bool IsIn = false;
+                    Color InPixel = Color.FromArgb(threadPixels[idx + 3], threadPixels[idx], threadPixels[idx + 1], threadPixels[idx + 2]);
+                    //The Pixel is "In", so we calculate the Distance to the edge
+                    if (InPixel == InColor)
+                    {
+                        IsIn = true;
+                    }
+
+                    int xMin = Math.Max(x - radius, 0);
+                    int xMax = Math.Min(x + radius, jobMaxWidth);
+                    int yMin = Math.Max(y - radius, 0);
+                    int yMax = Math.Min(y + radius, jobMaxHeight);
+
+                    //Now search in are around him
+                    for (int y2 = yMin; y2 < yMax; ++y2)
+                    {
+                        for (int x2 = xMin; x2 < xMax; ++x2)
+                        {
+                            int idx2 = (x2 + (y2 * jobMaxWidth)) * 4;
+                            Color OutPixel = Color.FromArgb(threadPixels[idx2 + 3], threadPixels[idx2], threadPixels[idx2 + 1], threadPixels[idx2 + 2]);
+                            if (IsIn && OutPixel != InColor || !IsIn && OutPixel == InColor)
+                            {
+                                // Calculate distance
+                                float xd = x - x2;
+                                float yd = y - y2;
+                                float d = (float)Math.Sqrt(Math.Abs((xd * xd) + (yd * yd)));
+
+                                // Compare absolute distance values, and if smaller replace distnace with the new oe
+                                if (d < distance)
+                                {
+                                    distance = d;
+                                }
+                            }
+
+
+                        }
+                    }
+
+                    //If we found a new distance, otherwise we'll just use A 
+                    float sAlpha = 0.0f;
+
+                    if (distance != float.MaxValue)
+                    {
+                        if (distance > radius)
+                            distance = radius;
+                        sAlpha = (radius - distance) / (radius * 2);
+                        if (IsIn)
+                        {
+                            sAlpha = 1.0f - sAlpha;
+                        }
+                    }
+                    else
+                    {
+                        if (IsIn)
+                        {
+                            sAlpha = 1.0f;
+                        }
+                    }
+
+                    // Write pixel out
+                    byte outVal = (byte)(sAlpha * 255);
+                    writePixels[idx] = 0;
+                    writePixels[idx + 1] = 0;
+                    writePixels[idx + 2] = 0;
+                    writePixels[idx + 3] = outVal;
+
+                    //Just for displaying where we are
+                    currentValue++;
+                }
+            }
+        }
+
+        private Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+        private void SeekJobs(ref byte[] resultPixels)
+        {
+            running++;
+            byte[] threadPixels;
+            GeneratePixelArray(ref loadedImage, out threadPixels, true);
+
+            while (currentJob != jobsCount)
+            {
+                int cjob = Interlocked.Increment(ref currentJob);
+                if (cjob != jobsCount)
+                {
+                    Job threadJob = Jobs[cjob];
+                    Console.WriteLine(cjob);
+                    SignedDistanceField(threadJob.minX, threadJob.minY, threadJob.maxX, threadJob.maxY, ref threadPixels, ref resultPixels);
+                }
+            }
+            running--;
+        }
+
+        #region Button Clicks
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            if (readDialog.ShowDialog() == DialogResult.OK)
+            {
+                imgPath = readDialog.FileName.ToString();
+                loadedImage = new Bitmap(imgPath);
+                //set display
+                imgpath.Text = imgPath;
+                imagebox.Image = loadedImage;
+                imagebox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                progressBar.Value = 0;
+                progress.Text = "Start Generation";
+            }
+            else
+            {
+                MessageBox.Show("File could not be loaded.");
+            }
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            if (imgPath == "")
+            {
+                MessageBox.Show("You have to load an Image first.");
+            }
+            else if (running <= 0)
+            {
+                startTime = DateTime.Now;
+
+                currentValue = 0;
+                radius = searchRadius.Value;
+                maxValue = loadedImage.Width * loadedImage.Height;
+                jobMaxWidth = loadedImage.Width;
+                jobMaxHeight = loadedImage.Height;
+
+                int traceJobWidth = jobMaxWidth / jobsHorizontal;
+                int traceJobHeight = jobMaxHeight / jobsVertical;
+
+                Jobs.Clear();
+
+                for (int y = 0; y < jobsVertical; ++y)
+                {
+                    for (int x = 0; x < jobsHorizontal; ++x)
+                    {
+                        Job job = new Job();
+                        job.minX = (int)(x * traceJobWidth);
+                        job.minY = (int)(y * traceJobHeight);
+                        job.maxX = (int)(job.minX + traceJobWidth);
+                        job.maxY = (int)(job.minY + traceJobHeight);
+                        Jobs.Add(job);
+                    }
+                }
+                
+                byte[] resultPixels;
+                GeneratePixelArray(ref loadedImage, out resultPixels, false);
+
+                Thread[] threads = new Thread[numCores];
+
+                //Start Threads
+                currentJob = -1;
+                for (int x = 0; x < numCores; ++x)
+                {
+                    threads[x] = new Thread(() => SeekJobs(ref resultPixels));
+                    threads[x].Start();
+                }
+                
+                while(running != 0)
+                {
+                    if (currentJob > 0)
+                    {
+                        string txt = Convert.ToString(currentJob) + "/ " + Convert.ToString(jobsCount);
+                        progress.Text = txt;
+                        int progressValue = (int)(((double)currentJob / (double)jobsCount) * 100);
+                        progressBar.Value = progressValue;
+                    }
+                    Application.DoEvents();
+                }
+                progressBar.Value = 100;
+
+                CopyToImage(ref resultPixels, ref loadedImage);
+
+                loadedImage = ResizeImage(loadedImage, (int)OutWidth.Value, (int)OutHeight.Value);
+                //set new image as display image
+                imagebox.Image = loadedImage;
+                progress.Text = "Generation complete.";
+
+                //UpdateStatus(ref resultPixels);
+            }
+        }
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (imgPath == "")
+            {
+                MessageBox.Show("You have to load an Image first.");
+            }
+            else
+            {
+                if (loadedImage != null && saveDialog.ShowDialog() == DialogResult.OK)
+                {
+                    loadedImage.Save(saveDialog.FileName);
+                    MessageBox.Show("Image saved!");
+                }
+            }
+        }
+
+        private void colorBox_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            MyDialog.AllowFullOpen = true;
+            MyDialog.AnyColor = true;
+            MyDialog.Color = colorBox.BackColor;
+
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+            {
+                colorBox.BackColor = MyDialog.Color;
+            }
+        }
+
+        private void searchRadius_Scroll(object sender, EventArgs e)
+        {
+            Radius.Text = "" + searchRadius.Value;
+        }
+
+        #endregion
+    }
+}
